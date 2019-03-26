c479f374f05baca9bb61ca3b27cf938987930dca
Luis Salazar <LuisMBA@Luiss-MacBook-Air.local>
2019-03-25 21:14:32 -0500

    implementation and adding some tasks to test

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-
 //Route
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
