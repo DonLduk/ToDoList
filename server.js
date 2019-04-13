bdd967ebe74cfc8e9561b008e7c9ff647258c09d
Luis Salazar <LuisMBA@Luiss-MacBook-Air.local>
2019-04-13 17:11:47 -0500

    added cors

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var express = require('express'),
   mongoose = require('mongoose'),
   Task = require('./api/models/todoListModel'), //created model loading here
   bodyParser = require('body-parser');
+
+ var cors = require('cors');
+
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
@@ -15,6 +18,21 @@ mongoose.connect('mongodb://localhost/Tododb');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 
 //Route
 var routes = require('./api/routes/todoListRoutes'); //importing route
